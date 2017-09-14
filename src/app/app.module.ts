9b5d1207e09fbb6defabfe0d368e97fe7ced0092
Erick <rick.mutua12@gmail.com>
2017-09-14 13:00:54 +0300

    added component details to hold details of the heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsComponent
   ],
 
   imports: [
