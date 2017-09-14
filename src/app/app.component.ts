04716c410886beaf5f28ff9809aec7d0f7ba7d4b
Erick <rick.mutua12@gmail.com>
2017-09-14 12:32:41 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Tour The Heroes';
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect (hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+};
+
+
+export class Hero {
+  id: number;
+  name: string;
 }
+
+
+const HEROES: Hero[] = [
+  { id:  1,   name: 'Iron Man' },
+  { id:  2,   name: 'Ninja Turles' },
+  { id:  3,   name: 'Harley Quinn' },
+  { id:  4,   name: 'Joker' },
+  { id:  5,   name: 'Ganta' },
+  { id:  6,   name: 'Todoroki' },
+  { id:  7,   name: 'All Might' },
+  { id:  8,   name: 'Inferno' },
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -7,10 +8,17 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent
   ],
+
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
+
 })
+
+
+
 export class AppModule { }
